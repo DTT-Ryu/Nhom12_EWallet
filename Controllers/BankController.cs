1cdae68c31077bfc52c4ebb84d0fd805dc2ae9ca
DTT-Ryu <thuongryu@gmail.com>
2025-03-25 14:21:31 +0700

    add controller + foler view

---
+++ Controllers/BankController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nhom12_EWallet.Controllers
+{
+    public class BankController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
