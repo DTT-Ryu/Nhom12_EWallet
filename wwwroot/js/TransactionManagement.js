05eae425fd60c72e0c3b73e364c85d290f9bcf56
DTT-Ryu <thuongryu@gmail.com>
2025-04-08 20:55:44 +0700

    done basic admin

--- wwwroot/js/TransactionManagement.js
+++ wwwroot/js/TransactionManagement.js
@@ -1,6 +1,50 @@
 ﻿$(document).ready(function () {
-    $('#btnDeleteTransaction').click(function () {
-        var transID = $(this).data('id');
+    $('.btnDeleteTransaction').click(function () {
+        var transID = $(this).data("id");
         console.log(transID);
+        $.ajax({
+            url: '/Admin/TransactionManagement/GetTransactionByID',
+            type: 'GET',
+            data: { id: transID },
+            success: function (response) {
+                if (response.success) {
+                    console.log(response);
+                    var t = response.trans;
+                    $('#deleteTransactionID').val(t.transactionId);
+                    if (t.transactionType == "deposit") {
+                        $('#deleteTranType').val("Nạp tiền");
+                    } else if (t.transactionType == "withdraw") {
+                        $('#deleteTranType').val("Rút tiền");
+                    } else if (t.transactionType == "transfer") {
+                        $('#deleteTranType').val("Chuyển tiền");
+                    }
+                    
+                    $('#deleteTranSender').val(t.senderUserPhone);
+                    if (t.transactionType == "transfer") {
+                        $('#deleteTranRecipient').val(t.recipientUserPhone);
+                    } else {
+                        $('#deleteTranRecipient').val(t.bankAccName);
+                    }
+                    
+                    $('#deleteTranAmount').val(t.amount);
+                    $('#deleteTranDescript').val(t.description);
+                    $('#deleteTranCreate').val(t.createAt);
+
+                    if (t.status == "pending") {
+                        $('#deleteTranStatus').val('Chưa giải quyết');
+                    } else if (t.status == "completed") {
+                        $('#deleteTranStatus').val('Hoàn thành');
+                    } else {
+                        $('#deleteTranStatus').val('Thất bại');
+                    }
+                    
+                } else {
+                    alert('Không tìm thấy giao dịch');
+                }
+            },
+            error: function (error) {
+                alert('Error load transaction: ' + error);
+            }
+        });
     });
 });
